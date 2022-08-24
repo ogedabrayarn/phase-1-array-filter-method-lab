b0b8ef911ab5c877b1d83ccfd34c8ccb4dbd5efa
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-08-24 19:31:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
 // Code your solution here
-const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
-function findMatching(source, sought) {
-    return source.filter(
-      (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
-    );
-  }
-  findMatching();
+const drivers = [
+  {
+    name: 'Bobby',
+    hometown: 'Pittsburgh' },
+  {
+    name: 'Sammy',
+    hometown: 'New York' } ,
+  {
+    name: 'Sally',
+    hometown: 'Cleveland' },
+  {
+    name: 'Annette',
+    hometown: 'Los Angeles' },
+  {
+    name: 'Bobby',
+    hometown: 'Tampa Bay' }
+];
+function findMatching(drivers, name) {
+
+  return drivers
+    .filter(function (driver) {
+      return driver.toLowerCase().includes(name.toLowerCase());
+    })
+}
+
+function fuzzyMatch(drivers, name) {
+  return drivers
+    .filter(function (driver) {
+      return driver.toLowerCase().startsWith(name.toLowerCase());
+    })
+}
+
+function matchName(drivers, name) {
+  return drivers
+    .filter(function (driver) {
+      return driver.name.toLowerCase() === name.toLowerCase();
+    })
+}
+
   
-  function fuzzyMatch(source, testString) {
-    return source.filter(
-      (possibleMatch) =>
-        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
-    );
-  }
-  
-  function matchName(source, soughtName) {
-    const drivers = [
-        {
-          name: 'Bobby',
-          hometown: 'Pittsburgh' },
-        {
-          name: 'Sammy',
-          hometown: 'New York' } ,
-        {
-          name: 'Sally',
-          hometown: 'Cleveland' },
-        {
-          name: 'Annette',
-          hometown: 'Los Angeles' },
-        {
-          name: 'Bobby',
-          hometown: 'Tampa Bay' }
-      ];
-    return source.filter((record) => record.name === soughtName);
-  }
+  // function matchName(drivers, name) {
+  //   
+  //   return source.filter((record) => record.name === name);
+  // }
